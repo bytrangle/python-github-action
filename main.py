e316c60bce0776183cdf1f13bdd7d2ebd7920ac8
Trang Le <trang.thule@zoho.com>
2021-12-01 18:39:31 +0700

    debug: open event json file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from os import environ, getenv
+import json
 
 print(environ)
-print("Running Python script")
+filePath = getenv('GITHUB_EVENT_PATH', '/github/workflows/event.json')
+
+f = open(filePath, "r")
+print(f.read())
