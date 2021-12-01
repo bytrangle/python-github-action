bf532c96c69226ef8fb46fc1201ddf88d0e8a61d
Trang Le <trang.thule@zoho.com>
2021-12-01 18:45:09 +0700

    feat(script): read event JSON file

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import json
 print(environ)
 filePath = getenv('GITHUB_EVENT_PATH', '/github/workflows/event.json')
 
-f = open(filePath, "r")
-print(f.read())
+# f = open(filePath, "r")
+# print(f.read())
+with open(filePath) as f:
+  data = json.load(f)
+print(data["issue"])
