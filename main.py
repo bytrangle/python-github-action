a64cfa2274c37fd7193cb9ed6ea0ee6450057899
Trang Le <trang.thule@zoho.com>
2021-12-02 14:26:52 +0700

    debug: pass Github token

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ with open(filePath) as f:
   data = json.load(f)
 print(data["issue"]["html_url"])
 
-if getenv('GITHUB_TOKEN') is not None:
+token = getenv('GITHUB_TOKEN')
+if token is not None:
   print('github token is available')
